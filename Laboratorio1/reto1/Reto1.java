d66e9744f734ef6d7c9dd318d7a36b1956422d7a
Juan Manuel Villegas <juan.vmedina@mail.escuelaing.edu.co>
2026-01-27 14:47:12 -0500

    solucion reto 1

---
+++ Laboratorio1/reto1/Reto1.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Reto1 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Estudiante {
+    String nombre;
+    int edad;
+    int semestre;
+    String correo;
+
+    public Estudiante(String nombre, int edad, int semestre, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.semestre = semestre;
+        this.correo = correo;
+    }
+}
