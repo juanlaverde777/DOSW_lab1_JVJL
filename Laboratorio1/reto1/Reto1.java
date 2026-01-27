47872cbf5fb621d95c6abe118fe16970c8a61d21
juanlaverde777 <josefolaverde@gmail.com>
2026-01-27 14:47:12 -0500

    solucion reto1

---
+++ Laboratorio1/reto1/Reto1.java
@@ -0,0 +1,27 @@
+mport java.util.List;
+import java.util.stream.Collectors;
+ 
+public class Reto1 {
+ 
+    public static void main(String[] args) {
+ 
+        List<Estudiante> estudiantes = List.of(
+            new Estudiante("Juan Manuel Villegas", 23, 6, "juan.villegas@mail.escuelaing.edu.co"),
+            new Estudiante("Juan Jose Laverde", 22, 6, "juan.laverde@mail.escuelaing.edu.co")
+        );
+ 
+        MensajeBienvenida bienvenida = (lista) ->
+            "¡Hola, bienvenidos! " +
+            lista.stream()
+                .map(e -> e.nombre)
+                .collect(Collectors.joining(" y "));
+ 
+        System.out.println(bienvenida.generar(estudiantes));
+    }
+}
+ 
+// Interfaz funcional
+@FunctionalInterface
+interface MensajeBienvenida {
+    String generar(List<Estudiante> estudiantes);
+}
