dd5e608bad2e5c166d845da4943786dda0a182a7
juanlaverde777 <josefolaverde@gmail.com>
2026-01-28 12:15:25 -0500

    implementacion minimo y cantidad carril 2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -2,30 +2,40 @@ package reto2;
  
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
  
 public class CarreraParalela {
     public static void main(String[] args) {
-        
+
         List<Integer> numeros = Arrays.asList(5, 12, 3, 8, 21, 7, 15);
-        /
-        Function<List<Integer>, Integer> calcularMaximo = lista -> 
-            lista.stream()
-                 .max(Integer::compareTo)
-                 .orElse(0);
-      
-        Function<List<Integer>, Integer> calcularMinimo = lista -> 
-            lista.stream()
-                 .min(Integer::compareTo)
-                 .orElse(0);
-       
-        Function<List<Integer>, Integer> contarDatos = lista -> lista.size();
-      
-        Integer maximo = calcularMaximo.apply(numeros);
-        Integer minimo = calcularMinimo.apply(numeros);
-        Integer cantidad = contarDatos.apply(numeros);
-        System.out.println("Número más grande: " + maximo);
-        System.out.println("Número más pequeño: " + minimo);
-        System.out.println("Cantidad de datos: " + cantidad);
+
+        Resultados res = procesarDatos(numeros);
+        System.out.println("Resultados (Carril 2 - Laverde): " + res);
+    }
+
+    public static Resultados procesarDatos(List<Integer> lista) {
+
+        int min = lista.stream().min(Integer::compareTo).orElse(0);
+
+        int cantidad = lista.size();
+
+        return new Resultados(0, min, cantidad);
+    }
+}
+ 
+
+class Resultados {
+    int max;
+    int min;
+    int cantidad;
+ 
+    public Resultados(int max, int min, int cantidad) {
+        this.max = max;
+        this.min = min;
+        this.cantidad = cantidad;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Máximo: " + max + ", Mínimo: " + min + ", Cantidad: " + cantidad;
     }
 }
