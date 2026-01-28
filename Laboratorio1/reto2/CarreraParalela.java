7462d0729679367a4a4b02aec83d8ade05506297
juanlaverde777 <josefolaverde@gmail.com>
2026-01-28 02:37:25 -0500

    feature: lambda para calcular número más pequeño y cantidad de datos - carril 2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,23 +1,31 @@
 package reto2;
-
+ 
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-
+ 
 public class CarreraParalela {
-    
     public static void main(String[] args) {
-        // Lista de prueba
-        List<Integer> numeros = Arrays.asList(5, 12, 3, 8, 21, 7, 15);
         
-        // Lambda para calcular el número más grande
+        List<Integer> numeros = Arrays.asList(5, 12, 3, 8, 21, 7, 15);
+        /
         Function<List<Integer>, Integer> calcularMaximo = lista -> 
             lista.stream()
                  .max(Integer::compareTo)
                  .orElse(0);
-        
-        // Probar la función
+      
+        Function<List<Integer>, Integer> calcularMinimo = lista -> 
+            lista.stream()
+                 .min(Integer::compareTo)
+                 .orElse(0);
+       
+        Function<List<Integer>, Integer> contarDatos = lista -> lista.size();
+      
         Integer maximo = calcularMaximo.apply(numeros);
+        Integer minimo = calcularMinimo.apply(numeros);
+        Integer cantidad = contarDatos.apply(numeros);
         System.out.println("Número más grande: " + maximo);
+        System.out.println("Número más pequeño: " + minimo);
+        System.out.println("Cantidad de datos: " + cantidad);
     }
 }
