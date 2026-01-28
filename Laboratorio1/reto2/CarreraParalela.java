1af59c3126466ce7c583b596d7c5a87b4f63b7e2
Juan Manuel Villegas <juan.vmedina@mail.escuelaing.edu.co>
2026-01-28 12:17:06 -0500

    Implementacion Maximo carril 1

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,31 +1,37 @@
 package reto2;
- 
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
- 
+
 public class CarreraParalela {
     public static void main(String[] args) {
-        
         List<Integer> numeros = Arrays.asList(5, 12, 3, 8, 21, 7, 15);
-        /
-        Function<List<Integer>, Integer> calcularMaximo = lista -> 
-            lista.stream()
-                 .max(Integer::compareTo)
-                 .orElse(0);
-      
-        Function<List<Integer>, Integer> calcularMinimo = lista -> 
-            lista.stream()
-                 .min(Integer::compareTo)
-                 .orElse(0);
-       
-        Function<List<Integer>, Integer> contarDatos = lista -> lista.size();
-      
-        Integer maximo = calcularMaximo.apply(numeros);
-        Integer minimo = calcularMinimo.apply(numeros);
-        Integer cantidad = contarDatos.apply(numeros);
-        System.out.println("Número más grande: " + maximo);
-        System.out.println("Número más pequeño: " + minimo);
-        System.out.println("Cantidad de datos: " + cantidad);
+        
+ 
+        Resultados res = procesarDatos(numeros);
+        
+        System.out.println("Resultados (Carril 1): " + res);
+    }
+
+   
+
+public static Resultados procesarDatos(List<Integer> lista) {
+    int max = lista.stream().max(Integer::compareTo).orElse(0);
+    return new Resultados(max, 0, 0);
+}
+}
+
+class Resultados {
+    int max, min, cantidad;
+    public Resultados(int max, int min, int cantidad) {
+        this.max = max; this.min = min; this.cantidad = cantidad;
+    }
+    @Override
+    public String toString() {
+        return "Máximo: " + max + ", Mínimo: " + min + ", Cantidad: " + cantidad;
     }
 }
+
+
+
+
