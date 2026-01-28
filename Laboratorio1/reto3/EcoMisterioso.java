9466ce49e0373ef3113162a15e4268311bc2107e
Juan Manuel Villegas <juan.vmedina@mail.escuelaing.edu.co>
2026-01-28 14:07:18 -0500

    Correccion codigo

--- Laboratorio1/reto3/EcoMisterioso.java
+++ Laboratorio1/reto3/EcoMisterioso.java
@@ -3,10 +3,9 @@ import java.util.function.UnaryOperator;
 
 public class EcoMisterioso {
     public static void main(String[] args) {
-        // Estudiante A define su lógica aquí
+       
         UnaryOperator<String> eco = mensaje -> {
             StringBuilder sb = new StringBuilder();
-            // Usamos Stream para repetir 3 veces
             IntStream.range(0, 3).forEach(i -> sb.append(mensaje).append(" "));
             return sb.toString().trim();
         };
